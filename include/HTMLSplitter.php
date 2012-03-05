6fbded8f898a6de53a81be17c68e2eb3187650e6
spectraweb <spectraweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-05 17:32:37 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-auto-columns/trunk@514803 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/HTMLSplitter.php
@@ -0,0 +1,287 @@
+<?php
+
+class HTMLSplitter
+{
+
+	protected $skip;
+	protected $pass;
+	protected $modifiers;
+	protected $headers;
+	protected $splittable;
+	protected $break;
+
+	/**
+	 *
+	 */
+	public function __construct()
+	{
+		$this->skip = array('head');
+		$this->pass = array('#document', 'html', 'body');
+		$this->modifiers = array(
+			'#default' => array('line-count' => 60, 'line-height' => 1, 'margin-bottom' => 0),
+			'h1' => array('line-count' => 20, 'line-height' => 2.5, 'margin-bottom' => 1),
+			'h2' => array('line-count' => 25, 'line-height' => 2, 'margin-bottom' => 1),
+			'h3' => array('line-count' => 40, 'line-height' => 1.5, 'margin-bottom' => 1),
+			'br' => array('margin-bottom' => 1),
+			'hr' => array('margin-bottom' => 1),
+			'ul' => array('margin-bottom' => 1),
+			'ol' => array('margin-bottom' => 1),
+			'p' => array('margin-bottom' => 1),
+		);
+		$this->headers = array('h1', 'h2', 'h3', 'h4', 'h5', 'h6');
+		$this->splittable = array('div', 'p', 'ul');
+		$this->break = array('br');
+	}
+
+	/**
+	 *
+	 */
+	public function setModifiers($modifiers)
+	{
+		$this->modifiers = array_merge($this->modifiers, $modifiers);
+	}
+
+	/**
+	 *
+	 */
+	public function split($html, $columns = 2)
+	{
+		$doc = new DOMDocument();
+
+		$doc->loadHTML('<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8"></head><body>' . $html . '</body></html>');
+
+		$nodeArray = $this->processNode($doc);
+
+		// calculate
+		$totalHeight = $this->calculateHeight($nodeArray, $this->modifiers['#default']);
+
+		$colHeight = floor($totalHeight / $columns);
+
+		//
+		$cols = array();
+
+		$column = 0;
+		$height = 0;
+		while (count($nodeArray) > 0)
+		{
+			$node = array_shift($nodeArray);
+
+			if (in_array($node->name, $this->splittable))
+			{
+				$current_tag = $node->name;
+
+				$children = array();
+				foreach ($node->children as $childNode)
+				{
+					// skip break tags in the beginning of the column
+					if (in_array($childNode->name, $this->break) && count($children) == 0)
+						continue;
+
+					// add text representation of the child node
+					$children[] = $childNode->node->C14N(false);
+					$height += $childNode->height;
+
+					// check we have to start new column
+					if ($height >= $colHeight)
+					{
+						// construct node
+						$nodeText = $this->constructStartTag($node) . implode('', $children) . '</' . $current_tag . '>';
+						$cols[$column][] = $nodeText;
+						$children = array();
+
+						// next column
+						$column++;
+						$cols[$column] = array();
+						$height = 0;
+					}
+				}
+
+				// if we have untouched children
+				if (count($children) > 0)
+				{
+					// construct node
+					$nodeText = $this->constructStartTag($node) . implode('', $children) . '</' . $current_tag . '>';
+					$cols[$column][] = $nodeText;
+
+					if ($height >= $colHeight)
+					{
+						// next column
+						$column++;
+						$cols[$column] = array();
+						$height = 0;
+					}
+				}
+			}
+			else
+			{
+				$cols[$column][] = $node->node->C14N(false);
+				$height += $node->height;
+
+				$header = in_array($node->name, $this->headers);
+				if (!$header && $height >= $colHeight)
+				{
+					// next column
+					$column++;
+					$cols[$column] = array();
+					$height = 0;
+				}
+			}
+		}
+
+		// fill last columns if can
+		if (count($cols[$columns - 1]) == 0 && count($cols[$columns - 2]) > 1)
+		{
+			array_push($cols[$columns - 1], array_pop($cols[$columns - 2]));
+		}
+
+		return $cols;
+	}
+
+	/**
+	 * Construct html opening tag for splitted nodes
+	 *
+	 * @param type $node
+	 * @return string
+	 */
+	protected function constructStartTag($node)
+	{
+		$ret = '<';
+		$ret .= $node->name;
+
+		if ($node->node->hasAttributes())
+		{
+			$attributes = array();
+
+			$length = $node->node->attributes->length;
+			for ($i = 0; $i < $length; $i++)
+			{
+				$attr = $node->node->attributes->item($i);
+				$attributes[] = $attr->name . '="' . $attr->value . '"';
+			}
+
+			if (count($attributes) > 0)
+			{
+				$ret .= ' ' . implode(' ', $attributes);
+			}
+		}
+
+		$ret .= '>';
+
+		return $ret;
+	}
+
+	/**
+	 *
+	 * @param type $node
+	 * @return null|\stdClass
+	 */
+	protected function processNode($node)
+	{
+		$name = $node->nodeName;
+
+		if (in_array($name, $this->skip))
+		{
+			return null;
+		}
+		else if (in_array($name, $this->pass))
+		{
+			return $this->getChildArray($node);
+		}
+
+		$n = new stdClass();
+		$n->name = $name;
+		$n->height = 0;
+		$n->node = $node;
+
+		if ($name == '#text')
+		{
+			// check text nodes
+			$text = trim($node->textContent);
+			if ($text == '')
+			{
+				// skip empty text nodes
+				return null;
+			}
+			$n->text = $text;
+		}
+
+		// process child nodes if any
+		if ($node->hasChildNodes())
+		{
+			$n->children = $this->getChildArray($node);
+		}
+
+		return $n;
+	}
+
+	/**
+	 *
+	 */
+	protected function getChildArray($node)
+	{
+		$ret = array();
+
+		if ($node->hasChildNodes())
+		{
+			// convert to array of child nodes
+			foreach ($node->childNodes as $childNode)
+			{
+				$c = $this->processNode($childNode);
+
+				if (is_array($c))
+				{
+					$ret = array_merge($ret, $c);
+				}
+				else if ($c != null)
+				{
+					$ret[] = $c;
+				}
+			}
+		}
+
+		return $ret;
+	}
+
+	/**
+	 *
+	 * @param type $nodeArray
+	 * @param type $params
+	 * @return type
+	 */
+	protected function calculateHeight($nodeArray, $params)
+	{
+		$height = 0;
+		foreach ($nodeArray as $node)
+		{
+			$name = $node->name;
+
+			// update params
+			$modifier = $this->modifiers[$name];
+			if ($modifier)
+			{
+				$nodeParams = array_merge($params, $modifier);
+			}
+			else
+			{
+				$nodeParams = $params;
+			}
+
+			//
+			if (count($node->children) > 0)
+			{
+				$node->height += $this->calculateHeight($node->children, $nodeParams);
+			}
+			else if ($node->name == '#text')
+			{
+				$lines = ceil(strlen($node->text) / $params['line-count']);
+
+				$node->height += ($lines * $params['line-height']) + $params['margin-bottom'];
+			}
+
+			$height += $node->height;
+		}
+
+		return $height;
+	}
+
+}
---
+++ wp-auto-columns.php
@@ -0,0 +1,126 @@
+<?php
+
+/*
+  Plugin Name: WP Auto Columns
+  Plugin URI: http://wordpress.org/extend/plugins/wp-auto-columns/
+  Description: @todo provide description
+  Author: Spectraweb s.r.o.
+  Author URI: http://www.spectraweb.cz
+  Version: 1.0.0
+ */
+
+load_plugin_textdomain('wp-auto-columns', false, dirname(plugin_basename(__FILE__)) . '/languages');
+
+register_activation_hook(__FILE__, array('WPAutoColumns', 'on_activation'));
+register_deactivation_hook(__FILE__, array('WPAutoColumns', 'on_deactivation'));
+
+add_filter('init', array('WPAutoColumns', 'on_init'));
+
+require_once('include/HTMLSplitter.php');
+
+class WPAutoColumns
+{
+
+	/**
+	 * plugin activation hook
+	 */
+	public static function on_activation()
+	{
+
+	}
+
+	/**
+	 * plugin deactivation hook
+	 */
+	public static function on_deactivation()
+	{
+
+	}
+
+	/**
+	 * plugin initialization
+	 */
+	public static function on_init()
+	{
+		if (is_admin())
+		{
+			add_action('admin_init', array('WPAutoColumns', 'on_admin_init'));
+			add_action('admin_menu', array('WPAutoColumns', 'on_admin_menu'));
+		}
+		else
+		{
+			wp_enqueue_style('auto_columns', plugin_dir_url(__FILE__) . 'css/auto-columns.css', array(), '1.0');
+
+			add_shortcode('auto_columns', array('WPAutoColumns', 'shortcode'));
+		}
+	}
+
+	/**
+	 *
+	 */
+	public static function on_admin_init()
+	{
+		//register_setting('wp_fetch_page_settings', 'wp_fetch_tm_currency');
+	}
+
+	/**
+	 *
+	 */
+	public static function on_admin_menu()
+	{
+		//add_options_page(__('Auto Columns Options', 'theme'), __('Auto Columns', 'theme'), 'manage_options', basename(__FILE__), array('WPAutoColumns', 'on_settings'));
+	}
+
+	/**
+	 *
+	 * @param type $atts
+	 * @param type $content
+	 */
+	public static function shortcode($atts, $content = '')
+	{
+		extract(shortcode_atts(array('columns' => 2), $atts));
+
+		$splitter = new HTMLSplitter();
+
+		$col_array = $splitter->split($content, $columns);
+
+		// construct container
+		$class = array('auto-columns-container', 'columns-' . $columns);
+		$ret = '<div class="' . implode(' ', $class) . '">';
+
+		for ($i = 0; $i < count($col_array); $i++)
+		{
+			// construct column class
+			$class = array('auto-columns-column', 'column-' . ($i + 1));
+			if ($i == 0)
+			{
+				$class[] = 'first-column';
+			}
+			if ($i == count($col_array) - 1)
+			{
+				$class[] = 'last-column';
+			}
+			// add column
+			$ret .= '<div class="' . implode(' ', $class) . '">';
+			foreach ($col_array[$i] as $tag)
+			{
+				$ret .= $tag;
+			}
+			$ret .= '</div>';
+		}
+
+		$ret .= '<div class="clear"></div>';
+		$ret .= '</div>';
+
+		return $ret;
+	}
+
+	/**
+	 *
+	 */
+	public static function on_settings()
+	{
+		echo 'settings';
+	}
+
+}
